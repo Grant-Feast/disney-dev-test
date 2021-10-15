a874a52db4e882e2510daa2d04b75bb60e20712e
Grant <feast5801@hotmail.co.uk>
2021-10-15 11:28:08 +0100

    create nav, slider and update sections

---
+++ app.js
@@ -0,0 +1,9 @@
+// Navigation JS code
+let mainNav = document.querySelector('#js-menu');
+let navBarToggle = document.querySelector('#js-navbar-toggle');
+
+// When user clicks on the toggle icon, a class is added to display nav items.
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
+
