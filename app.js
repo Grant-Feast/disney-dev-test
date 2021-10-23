fc171e4aa8c448ad425103652d367716165a09de
Grant <feast5801@hotmail.co.uk>
2021-10-23 11:12:30 +0100

    change input placeholder when selected

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-// Navigation JS code
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+let mainNav = document.querySelector('#js-menu');
+let navBarToggle = document.querySelector('#js-navbar-toggle');
+let fName = document.querySelector('#f-name');
+let lName = document.querySelector('#l-name');
+let number = document.querySelector('#number');
+let email = document.querySelector('#email');
+let message = document.querySelector('#message');
 
+// Navigation JS code
 // When user clicks on the toggle icon, a class is added to display nav items.
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
 
+// Change the placeholder text on input fields when clicked
+fName.addEventListener('click', function () {
+  fName.placeholder = 'Entering name...';
+});
+
+lName.addEventListener('click', function () {
+  lName.placeholder = 'Entering surname...';
+});
+
+number.addEventListener('click', function () {
+  number.placeholder = 'Entering phone number...';
+});
+
+email.addEventListener('click', function () {
+  email.placeholder = 'Entering email...'
+});
+
+message.addEventListener('click', function () {
+  message.placeholder = 'Entering message...'
+});
